97ee8aac7b3c076f88df0d623d3562576c40ca86
Ahmad Asmandar <ahmedasmndr2@gmail.com>
2021-01-10 20:50:08 +0100

    update clean script

--- clean.py
+++ clean.py
@@ -23,45 +23,97 @@ for item in content:
                 # if (line.strip("\n") != "þStartfischer1 p33-6 hf8 fc6x 0.9km")
                 if ("Ch" in line.strip("\n") or "V:" in line.strip("\n")):
                     f.write(line.replace(';', ''))
-print("wer are done.... have fun!! KOMPASS-SENSOR")
-a={}
+print("we are done.... have fun!! KOMPASS-SENSOR")
+
+############################################
+
+a = {}
+lst = {}
 content = os.listdir("./cleandata")
-result=pd.DataFrame()
-verhaeltnis=pd.DataFrame()
+result = pd.DataFrame()
+verhaeltnis = pd.DataFrame()
+deltas = pd.DataFrame()
 for x in content:
     if (x.find(".txt") > 0 and x != ".git"):
-        #print(x)
-        df = pd.read_csv("./cleandata/{}".format(x),names=["channal", "min", "max", "x", "y"],sep="\s+",encoding="cp1258")
+        # print(x)
+        df = pd.read_csv("./cleandata/{}".format(x), names=[
+                         "channal", "min", "max", "x", "y"], sep="\s+", encoding="ISO-8859-1")
         dx = df.drop(["x", "y"], axis=1)
-        dx["min"] = dx["min"]  # .map(lambda x: x.lstrip("+-;").rstrip("aAbBcC;"))
-        dx["channal"] = dx["channal"]  # .map(lambda x: x.lstrip(";").rstrip(";"))
+        # .map(lambda x: x.lstrip("+-;").rstrip("aAbBcC;"))
+        dx["min"] = dx["min"]
+        # .map(lambda x: x.lstrip(";").rstrip(";"))
+        dx["channal"] = dx["channal"]
         if 100 in dx.index:
             dx = dx.drop(100)
         dx["min"] = pd.to_numeric(dx["min"])
         dx["max"] = pd.to_numeric(dx["max"])
         dx["deltas"] = dx["max"] - dx["min"]
-        dv=dx[dx.channal == "V:"]
-        dv.index.name=x.strip('.txt')
-        dx.index.name = x.strip('.txt')
-        dx=dx.reset_index()
-        #dv=dv.drop(["min", "max","deltas"], axis=1)#.reset_index()
-        dv=dv.reset_index()
-        if not (dx.empty):
-            a[x]=dv
-        dv=dv.replace('V:', 'V:{}'.format(x.strip('.txt')))
-        #drop empty Dataframes
-        #print(dv.head())
-        #dx.append(pd.Series(name='NameOfNewRow'))
-        if not (dx.empty):
-            result=pd.concat([result,dx.append(pd.Series(name='Verh..'))],axis=1)
-            verhaeltnis=pd.concat([verhaeltnis,dv],axis=1)
-            new_df=result.append(verhaeltnis)
+        ########### Extract channals ######
 
-#new_df=pd.concat([result,verhaeltnis])
-#print(result.head())
+        ch0 = dx[dx.channal == "Ch0:"]
+        ch1 = dx[dx.channal == "Ch1:"]
+        ch2 = dx[dx.channal == "Ch2:"]
+        ch3 = dx[dx.channal == "Ch3:"]
+        ch4 = dx[dx.channal == "Ch4:"]
+        ch5 = dx[dx.channal == "Ch5:"]
+        ch6 = dx[dx.channal == "Ch6:"]
+        ch7 = dx[dx.channal == "Ch7:"]
+        ch8 = dx[dx.channal == "Ch8:"]
+        # print(ch0.head())
+        ############ build the deltas #############
+        delta0 = ch0["max"] - ch0["min"]
+        lst['delta0'] = delta0
+        delta1 = ch1["max"] - ch1["min"]
+        lst['delta1'] = delta1
+        delta2 = ch2["max"] - ch2["min"]
+        lst['delta2'] = delta2
+        delta3 = ch3["max"] - ch3["min"]
+        lst['delta3'] = delta3
+        delta4 = ch4["max"] - ch4["min"]
+        lst['delta4'] = delta4
+        delta5 = ch5["max"] - ch5["min"]
+        lst['delta5'] = delta5
+        delta6 = ch6["max"] - ch6["min"]
+        lst['delta6'] = delta6
+        delta7 = ch7["max"] - ch7["min"]
+        lst['delta7'] = delta7
+        delta8 = ch8["max"] - ch8["min"]
+        lst['delta8'] = delta8
+        # print(delta0.head())
+        # format reset index
+
+        df_new = pd.DataFrame.from_dict(lst)
+        df_new = df_new.apply(lambda x: pd.Series(x.dropna().values))
+        # print(df_new.head(3))
+        df_new.to_excel("dfnew.xlsx")
+        dv = dx[dx.channal == "V:"]
+
+        # connect the name of Measurement
+        dv.index.name = x.strip('.txt')
+        dx.index.name = x.strip('.txt')
+        df_new.index.name = x.strip('.txt')
+
+        dx = dx.reset_index()
+        dv = dv.reset_index()
+        df_new = df_new.reset_index()
+
+        if not (dx.empty):
+            a[x.strip('.txt')] = dx
+        dv = dv.replace('V:', 'V:{}'.format(x.strip('.txt')))
+        # drop empty Dataframes
+        if not (dx.empty):
+            result = pd.concat(
+                [result, dx.append(pd.Series(name='Verh..',dtype=pd.StringDtype()))], axis=1)
+            verhaeltnis = pd.concat([verhaeltnis, dv], axis=1)
+            new_df = result.append(verhaeltnis)
+            deltas = pd.concat([deltas, df_new], axis=1)
+
+# new_df=pd.concat([result,verhaeltnis])
+# print(result.head())
 result.to_excel("result.xlsx")
-verhaeltnis=verhaeltnis.drop(["max","deltas"], axis=1)
-verhaeltnis=verhaeltnis.rename(columns={"min": "V"})
+verhaeltnis = verhaeltnis.drop(["max", "deltas"], axis=1)
+verhaeltnis = verhaeltnis.rename(columns={"min": "V"})
 verhaeltnis.head()
 verhaeltnis.to_excel("verhaeltnis.xlsx")
 new_df.to_excel("FullData.xlsx")
+deltas.to_excel("deltas.xlsx")
